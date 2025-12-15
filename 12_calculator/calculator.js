db9f058fd25a11a3f0f41c64ed11a93a1fbcef60
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2025-12-15 16:25:58 -0500

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((sum, number) => sum * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	// Catch negatives even if that'd be invalid
+  if (a <= 0) {return 1};
+  currentProduct = 1;
+  for (let i = a; i > 0; i--) {
+    currentProduct *= i;
+  }
+  return currentProduct;
 };
 
 // Do not edit below this line
