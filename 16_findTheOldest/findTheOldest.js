66d9697a8c7758a51181bcd762a806f3f6330c13
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2025-12-15 18:23:03 -0500

    Complete Assignment 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldest = 0;
+    let oldestPerson = {};
+    let death = 0;
+    array.forEach(person => {
+        if (person.yearOfDeath === undefined) {
+            death = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+        else {
+            death = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (death > oldest) {
+            oldest = death;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
