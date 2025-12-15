ebcc5fbb84e34e4305134518f8faf7ae0f5885da
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2025-12-15 17:38:10 -0500

    Complete assignment 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {return "OOPS"};
+    if (num === 0) {return 0};
+    if (num === 1) {return 1};
+    first = 1;
+    second = 1;
+    for (let i = num; i > 2; i--) {
+        [first, second] = [second, second + first];
+    }
+    return second;
 };
 
 // Do not edit below this line
