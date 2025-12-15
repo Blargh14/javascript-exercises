81c88e6f78d3cbdf9064adafe9d18020a93ad656
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2025-12-15 17:04:37 -0500

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.toLowerCase()
+        .replace(/ |,|\.|;|\?|!|'/g, "") // Excuse the regex
+    reverse = text.split("")
+        .reverse()
+        .join("");
+    return text === reverse;
 };
 
 // Do not edit below this line
